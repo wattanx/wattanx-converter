db2164ee6fdd0a225057794f30e2eda2be21719f
wattanx <wattan.dev@gmail.com>
2023-02-17 14:38:43 +0900

    fix: add custom validator test

--- packages/vue-script-setup-converter/src/lib/converter/propsConverter.test.ts
+++ packages/vue-script-setup-converter/src/lib/converter/propsConverter.test.ts
@@ -6,7 +6,7 @@ import parserTypeScript from "prettier/parser-typescript";
 import { getNodeByKind } from "../helper";
 import { convertProps } from "./propsConverter";
 
-const source = `<script lang="ts">
+const source = `<script>
   import { defineComponent, toRefs, computed, ref } from 'vue';
   
   export default defineComponent({
@@ -135,6 +135,67 @@ test("type-based defineProps with require and default pattern", () => {
 
   const expected = `type Props = { msg?: string; foo: string };
 const props = withDefaults(defineProps<Props>(), { msg: "HelloWorld" });
+`;
+
+  expect(formatedText).toBe(expected);
+});
+
+test("custom validator", () => {
+  const source = `<script>
+  import { defineComponent, toRefs, computed, ref } from 'vue';
+  
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        default: 'HelloWorld'
+      },
+      foo: {
+        type: String,
+        required: true,
+        validator(value) {
+          return ["success", "warning", "danger"].includes(value)
+        }
+      }
+    }
+  })
+  </script>`;
+  const {
+    descriptor: { script },
+  } = parse(source);
+
+  const project = new Project({
+    tsConfigFilePath: "tsconfig.json",
+    compilerOptions: {
+      target: ScriptTarget.Latest,
+    },
+  });
+
+  const sourceFile = project.createSourceFile("s.tsx", script?.content ?? "");
+
+  const callexpression = getNodeByKind(sourceFile, SyntaxKind.CallExpression);
+
+  const props = convertProps(callexpression as CallExpression);
+
+  const formatedText = prettier.format(props, {
+    parser: "typescript",
+    plugins: [parserTypeScript],
+  });
+
+  const expected = `const props = defineProps({
+  msg: {
+    type: String,
+    default: "HelloWorld",
+  },
+  foo: {
+    type: String,
+    required: true,
+    validator(value) {
+      return ["success", "warning", "danger"].includes(value);
+    },
+  },
+});
 `;
 
   expect(formatedText).toBe(expected);
