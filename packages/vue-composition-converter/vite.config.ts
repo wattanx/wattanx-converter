2919d63fbae0584cac4ec7890bdf6a5775bcb5bf
wattanx <wattan.dev@gmail.com>
2024-05-11 17:20:17 +0900

    chore: migrate to vitest workspace

--- packages/vue-composition-converter/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-import path from 'path';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  test: {
-    environment: 'node',
-  },
-});
