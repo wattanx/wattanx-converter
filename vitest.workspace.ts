64c53ffd962472000e72408936b971753bf63db4
wattanx <wattan.dev@gmail.com>
2025-04-14 09:28:42 +0900

    chore(deps): update vitest

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,10 +1,3 @@
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace([
-  "packages/*",
-  {
-    test: {
-      environment: "node",
-    },
-  },
-]);
+export default defineWorkspace(["packages/*"]);
